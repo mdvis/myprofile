63cf8a755a87cbe81c104884a2a7e20138569cdc
dewei.ma <xnmageis@outlook.com>
2025-02-11 16:43:17 +0800

    feat: mono

--- setup-brew.sh
+++ setup-brew.sh
@@ -16,7 +16,7 @@ brew install btop cmake ctags fastfetch fd ffmpeg ffmpegthumbnailer zsh wabt \
     font-symbols-only-nerd-font fuse-t fuse-t-sshfs fzf gcc gpg grex hadolint \
     highlight htop imagemagick jq lazygit libxml2 localsend make mkcert neovim \
     nnn node nss nvm openssl pandoc poppler ripgrep rust-analyzer scrcpy sevenzip \
-    shellcheck shfmt the_silver_searcher tmux tree yamllint zoxide
+    shellcheck shfmt the_silver_searcher tmux tree yamllint zoxide mono-mdk
 
 brew install --cask alt-tab another-redis-desktop-manager audacity ghostty windterm \
     balenaetcher blender calibre charles cryptomator dbeaver-community docker \
