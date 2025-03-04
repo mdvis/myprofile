d695ce07dea57c2c6d494251f9663c7d0c8d9cdf
dewei.ma <xnmageis@outlook.com>
2025-03-04 17:47:57 +0800

    feat: fish

--- setup-brew.sh
+++ setup-brew.sh
@@ -13,11 +13,11 @@ brew tap macos-fuse-t/homebrew-cask
 
 brew install \
     btop cmake ctags dprint fastfetch fd ffmpeg ffmpegthumbnailer fuse-t-sshfs \
-    fuse-t fzf gcc git-lfs go gpg grex hadolint highlight htop imagemagick zsh \
+    fuse-t yamllint fzf gcc git-lfs go gpg hadolint highlight htop imagemagick \
     jq lazygit libxml2 make mkcert neovim nnn node nss nvm openssl pandoc pnpm \
     prettier ripgrep ruff rust rust-analyzer scrcpy sevenzip shellcheck zoxide \
     shfmt sqlfluff stylelint stylua the_silver_searcher tmux tree wabt poppler \
-    yazi yamllint font-symbols-only-nerd-font aria2 atuin
+    grex yazi font-symbols-only-nerd-font aria2 atuin fish
 
 brew install --cask \
     alt-tab android-studio another-redis-desktop-manager arc audacity windterm \
