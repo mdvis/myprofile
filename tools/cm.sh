05468d0cc107cc111fb0631eb80032e17c788b6b
dewei.ma <xnmageis@outlook.com>
2025-03-04 17:42:09 +0800

    feat: del tools

--- tools/cm.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-# ------
-# name: comp.sh
-# author: Deve
-# date: 2023-10-17
-# ------
-
-find . -name '*.gltf' | sed 's/.gltf//' \
-    | xargs -I {} gltf-pipeline -i {}.gltf -o {}_comp.gltf -d
