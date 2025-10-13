767689a41f3a75908a0690109d86b46bf6a09ee1
dewei.ma <xnmageis@outlook.com>
2025-10-13 15:00:28 +0800

    feat: :hammer_and_wrench: aicli

--- aicli.sh
+++ aicli.sh
@@ -1,15 +1,40 @@
+#! /usr/bin/env bash
 # ------
 # name: ai_cli.sh
 # author: Deve
 # date: 2025-09-20
 # ------
 
-# @openai/codex
-# @volcengine/vecli
-# @continuedev/cli
-# @anthropic-ai/claude-code
-pnpm add --global \
-    @qwen-code/qwen-code@latest \
-    @google/gemini-cli \
-    opencode-ai@latest \
+list=(
+    @google/gemini-cli
+    @openai/codex
+    @anthropic-ai/claude-code
+    @continuedev/cli
+    opencode-ai
+    @qwen-code/qwen-code
     @charmland/crush
+    @github/copilot
+    @augmentcode/auggie
+    @qodo/command
+    cline
+)
+
+install() {
+    for pkg in "${list[@]}"; do
+        npm i -g "$pkg"
+    done
+}
+
+remove() {
+    for pkg in "${list[@]}"; do
+        npm un -g "$pkg"
+    done
+}
+
+if [[ "$1" = "install" ]]; then
+    install
+fi
+
+if [[ "$1" = "remove" ]]; then
+    remove
+fi
