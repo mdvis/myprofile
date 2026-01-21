a8a0d21b3c93d58e3aa0e8ca55c1b69533ea53b5
dewei.ma <xnmageis@outlook.com>
2026-01-21 16:59:06 +0800

    feat: :wrench: ollama translate

---
+++ tools/hyy.sh
@@ -0,0 +1,7 @@
+# ------
+# name: hyy.sh
+# author: Deve
+# date: 2026-01-21
+# ------
+
+ollama run translategemma "汉译英 \"$1\""
---
+++ tools/yyh.sh
@@ -0,0 +1,7 @@
+# ------
+# name: yyh.sh
+# author: Deve
+# date: 2026-01-21
+# ------
+
+ollama run translategemma "英译汉 \"$1\""
