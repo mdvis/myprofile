cc29d4d0849ca97f344ab2542da6c955e026ccc7
dewei.ma <xnmageis@outlook.com>
2026-02-02 13:27:43 +0800

    feat: :wrench: apt

---
+++ setup-apt.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# ------
+# name: setup-dnf.sh
+# author: Deve
+# date: 2022-06-29
+# ------
+
+APP_NAME="myprofile"
+APP_PATH="${HOME}/.${APP_NAME}"
+
+echo "------------ apt start ------------"
+while read -r app; do
+    sudo apt install -y "$app"
+done <"$APP_PATH"/packages/apt
+echo "------------  apt end  ------------"
